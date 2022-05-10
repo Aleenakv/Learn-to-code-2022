bd527a859ec95d22ceaad619048b6c2d5feb96e4
Aleenakv <96719731+Aleenakv@users.noreply.github.com>
2022-05-11 00:30:49 +0530

    Update Varieties everywhere.c

--- Varieties everywhere.c
+++ Varieties everywhere.c
@@ -4,15 +4,15 @@
 #include <stdlib.h>
 
 int main() {
-    int a;
-      char b;
-        float c;
-            scanf("%d\n",&a);
-            scanf("%c\n",&b);
-            scanf("%f\n",&c);
-        printf("%d\n",a);
-        printf("%c\n",b);
-        printf("%f\n",c);
+int a;
+char b;
+float c;
+scanf("%d\n",&a);
+scanf("%c\n",&b);
+scanf("%f\n",&c);
+printf("%d\n",a);
+printf("%c\n",b);
+printf("%f\n",c);
     
         
 
