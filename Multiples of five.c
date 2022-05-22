cb65d13bad22e88540c9215dcb02830c6ffdfadf
Aleenakv <96719731+Aleenakv@users.noreply.github.com>
2022-05-22 17:09:05 +0530

    Create Multiples of five.c

---
+++ Multiples of five.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i,multiples;
+    for(i=3;i<=19;i++)
+    {
+        multiples=i*5;
+        printf("%d\n",multiples);
+    }
+        
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
